abcef4fed44fab96549fac10bee3a68859808d95
Ritwik Ghosh <ritwikghosh765@gmail.com>
2018-03-20 15:51:21 +0100

    Solution to problem 4

--- problem-4/palindromeProduct.py
+++ problem-4/palindromeProduct.py
@@ -6,4 +6,38 @@ def numDigits(inputNum):
 		if inputNum < 1:
 			return placeValue
 
-print(numDigits(603))	
+def isPalindrome(pNum):
+	
+	nDigits = numDigits(pNum)
+	digits = []
+	
+	for num in range(1, nDigits + 1):
+		digits.append(int((pNum%10**num)/10**(num -1)))
+
+	for i in range(0, len(digits)):
+		if digits[i] != digits[(len(digits) -1) - i]:
+			return False
+
+	return True
+
+def bruteForceThruCombinations():
+	allPalindromes = [] 
+
+	for LHS in count(999, -1):
+		if LHS < 100:
+			break
+
+		for RHS in count(999, -1):
+			if RHS < 100 :
+				break
+			product = LHS*RHS
+			if(isPalindrome(product)):
+				allPalindromes.append(product)
+
+	return allPalindromes			
+
+allPalindromes = bruteForceThruCombinations()
+
+print(max(allPalindromes))
+
+	
